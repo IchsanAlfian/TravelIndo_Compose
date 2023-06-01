5573b8ce9cba1daf8ac8508329545eff2b7a9b68
IchsanAlfian <eksanalfian@gmail.com>
2023-06-01 21:28:38 +0700

    Detail Success

--- app/src/main/java/com/ichsanalfian/travelindocompose/TravelIndoApp.kt
+++ app/src/main/java/com/ichsanalfian/travelindocompose/TravelIndoApp.kt
@@ -10,15 +10,19 @@ import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.navigation.NavGraph.Companion.findStartDestination
 import androidx.navigation.NavHostController
+import androidx.navigation.NavType
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.currentBackStackEntryAsState
 import androidx.navigation.compose.rememberNavController
+import androidx.navigation.navArgument
 import com.ichsanalfian.travelindocompose.ui.theme.component.PlaceItem
 import com.ichsanalfian.travelindocompose.model.PlaceData
 import com.ichsanalfian.travelindocompose.ui.theme.TravelIndoComposeTheme
 import com.ichsanalfian.travelindocompose.ui.theme.navigation.Screen
+import com.ichsanalfian.travelindocompose.ui.theme.screen.detailPlace.DetailPlaceScreen
 import com.ichsanalfian.travelindocompose.ui.theme.screen.main.MainScreen
 
 @Composable
@@ -41,6 +45,19 @@ fun TravelIndoApp(
                     navigateToDetail = {
                         navController.navigate(Screen.DetailPlace.createRoute(it))
                     }
+                )
+            }
+            composable(
+                route = Screen.DetailPlace.route,
+                arguments = listOf(navArgument("placeId") { type = NavType.StringType }),
+            ){
+                val id = it.arguments?.getString("placeId") ?: -1L
+                DetailPlaceScreen(
+                    placeId = id as String,
+                    navigateBack = {
+                        navController.navigateUp()
+                    },
+
                 )
             }
         }
--- app/src/main/java/com/ichsanalfian/travelindocompose/ui/theme/navigation/Screen.kt
+++ app/src/main/java/com/ichsanalfian/travelindocompose/ui/theme/navigation/Screen.kt
@@ -3,6 +3,6 @@ package com.ichsanalfian.travelindocompose.ui.theme.navigation
 sealed class Screen(val route: String) {
     object Main : Screen("main")
     object DetailPlace : Screen("main/{placeId}") {
-        fun createRoute(placeId: String) = "home/$placeId"
+        fun createRoute(placeId: String) = "main/$placeId"
     }
 }
