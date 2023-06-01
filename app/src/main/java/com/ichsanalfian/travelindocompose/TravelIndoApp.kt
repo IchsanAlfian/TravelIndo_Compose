2b045e49ff7e70d044e96ae58b46e8c9959f1ad8
IchsanAlfian <eksanalfian@gmail.com>
2023-06-01 11:11:26 +0700

    Add Card On Lazy Grid

--- app/src/main/java/com/ichsanalfian/travelindocompose/TravelIndoApp.kt
+++ app/src/main/java/com/ichsanalfian/travelindocompose/TravelIndoApp.kt
@@ -1,23 +1,13 @@
 package com.ichsanalfian.travelindocompose
 
-import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
-import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.grid.GridCells
 import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
 import androidx.compose.foundation.lazy.grid.items
-import androidx.compose.foundation.lazy.items
-import androidx.compose.foundation.shape.CircleShape
-import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.clip
-import androidx.compose.ui.layout.ContentScale
-import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import coil.compose.AsyncImage
 import com.ichsanalfian.travelindocompose.component.PlaceItem
 import com.ichsanalfian.travelindocompose.model.PlaceData
 import com.ichsanalfian.travelindocompose.ui.theme.TravelIndoComposeTheme
--- app/src/main/java/com/ichsanalfian/travelindocompose/component/PlaceItem.kt
+++ app/src/main/java/com/ichsanalfian/travelindocompose/component/PlaceItem.kt
@@ -2,13 +2,12 @@ package com.ichsanalfian.travelindocompose.component
 
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
-import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material.Card
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.clip
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextOverflow
@@ -20,37 +19,53 @@ import com.ichsanalfian.travelindocompose.ui.theme.TravelIndoComposeTheme
 
 @Composable
 fun PlaceItem(
-    name : String,
-    location : String,
+    name: String,
+    location: String,
     photoUrl: String,
     modifier: Modifier = Modifier
-){
-    Column(
-        modifier = modifier.clickable {}
+) {
+    Card(
+        modifier = modifier.clickable { },
+        shape = Shapes.medium,
+        elevation = 4.dp
     ) {
-        AsyncImage(
-            model = photoUrl,
-            contentDescription = null,
-            contentScale = ContentScale.Crop,
-            modifier = Modifier
-                .size(170.dp)
-                .clip(Shapes.medium)
-        )
-        Text(
-            text = name,
-            overflow = TextOverflow.Ellipsis,
-            style = MaterialTheme.typography.subtitle1.copy(
-                fontWeight = FontWeight.ExtraBold
-            ),
-            modifier = Modifier.align(Alignment.CenterHorizontally)
-        )
-        Text(
-            text = location,
-            style = MaterialTheme.typography.subtitle2,
-            modifier = Modifier.align(Alignment.CenterHorizontally)
-        )
+        Column(
+            modifier = Modifier.padding(16.dp)
+        ) {
+            Box(
+                modifier = Modifier
+                    .height(120.dp)
+                    .fillMaxWidth()
+            ) {
+                AsyncImage(
+                    model = photoUrl,
+                    contentDescription = null,
+                    contentScale = ContentScale.Crop,
+                    modifier = Modifier.fillMaxSize()
+                )
+            }
+            Text(
+                text = name,
+                overflow = TextOverflow.Ellipsis,
+                maxLines = 2,
+                style = MaterialTheme.typography.subtitle1.copy(
+                    fontWeight = FontWeight.ExtraBold,
+                ),
+                modifier = Modifier
+                    .padding(top = 8.dp)
+                    .align(Alignment.CenterHorizontally)
+            )
+            Text(
+                text = location,
+                style = MaterialTheme.typography.subtitle2,
+                modifier = Modifier
+                    .padding(top = 8.dp)
+                    .align(Alignment.CenterHorizontally)
+            )
+        }
     }
 }
+
 @Preview(showBackground = true)
 @Composable
 fun PlaceListItemPreview(){
